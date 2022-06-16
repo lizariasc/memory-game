c9c550e2a2261279868bf07bcbdec7fd111aa867
Liz A <lizethariasc14@gmail.com>
2022-06-16 12:42:47 -0500

    suffle array

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const cardArray = [
     {
         name: 'fries',
-        image:
+        image: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        image: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        image: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-crean',
+        image: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        image: 'images/milk-shake.png'
+    },
+    {
+        name: 'pizza',
+        image: 'images/piza.png'
+    },
+    {
+        name: 'fries',
+        image: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        image: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        image: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-crean',
+        image: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        image: 'images/milk-shake.png'
+    },
+    {
+        name: 'pizza',
+        image: 'images/piza.png'
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random());
