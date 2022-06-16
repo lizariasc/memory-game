dcf9bb00e4bca7b582e4b67320e5a883fffa0822
Liz A <lizethariasc14@gmail.com>
2022-06-16 13:35:05 -0500

    check for a match

--- app.js
+++ app.js
@@ -52,7 +52,9 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
-const cardsChosen = [];
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardWon = [];
 
 // create an element for each item in the array
 function createBoard() {
@@ -68,13 +70,32 @@ function createBoard() {
 
 createBoard();
 
+function checkMath() {
+    const cards = document.querySelectorAll('img')
+    if(cardsChosen[0] == cardsChosen[1]) {
+        window.alert('you found a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png');
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png');
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard);
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard);
+        cardWon.push(cardsChosen)
+    }
+    cardsChosen = [];
+    cardsChosenIds = [];
+}
+
 function flipCard() {
     //get id of the card
     const cardId = this.getAttribute('data-id');
     //push the name to the new array
     cardsChosen.push(cardArray[cardId].name)
-    console.log(cardsChosen)
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen);
+    console.log(cardsChosenIds);
     
     // show the img when I flip the card
     this.setAttribute('src', cardArray[cardId].image)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMath, 500) 
+}
 }
